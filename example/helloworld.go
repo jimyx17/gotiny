09572d9b09fae7236c11df5acb2f33329f48808c
jimyx17 <jimyx17@gmail.com>
2020-01-05 11:55:59 +0100

    Fix hello world

--- example/helloworld.go
+++ example/helloworld.go
@@ -14,11 +14,11 @@ func main() {
 	gotiny.Unmarshal(d, &ret1, &ret2)
 	fmt.Println(ret1 + string(ret2)) // print "hello world!"
 
-	enc, _ := gotiny.NewEncoder(src1, src2)
-	dec, _ := gotiny.NewDecoder(ret1, ret2)
+	enc := gotiny.NewEncoder(src1, src2)
+	dec := gotiny.NewDecoder(ret1, ret2)
 
 	ret1, ret2 = "", []byte{3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 5, 6, 7, 44, 7, 5, 6, 4, 7}
-	o, _ := enc.EncodeValue(reflect.ValueOf(src1), reflect.ValueOf(src2))
+	o := enc.EncodeValue(reflect.ValueOf(src1), reflect.ValueOf(src2))
 	dec.DecodeValue(o,
 		reflect.ValueOf(&ret1).Elem(), reflect.ValueOf(&ret2).Elem())
 	fmt.Println(ret1 + string(ret2)) // print "hello world!"
